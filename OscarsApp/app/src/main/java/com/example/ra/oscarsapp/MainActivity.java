4497f856783483da8fb85b8effe78143a82e383e
Rcureton <rashad.cureton@gmail.com>
2016-02-29 11:51:54 -0500

    layouts completed

--- OscarsApp/app/src/main/java/com/example/ra/oscarsapp/MainActivity.java
+++ OscarsApp/app/src/main/java/com/example/ra/oscarsapp/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.ra.oscarsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 
 public class MainActivity extends AppCompatActivity {
+    ListView mListview;
+    ArrayAdapter<String> mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListview=(ListView)findViewById(R.id.lsitView);
+
+
 
 
     }
