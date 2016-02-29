96c86e4930c7d2e09ac6aa580f497bae83c88058
Rcureton <rashad.cureton@gmail.com>
2016-02-29 12:47:05 -0500

    added actors

--- OscarsApp/app/src/main/java/com/example/ra/oscarsapp/MainActivity.java
+++ OscarsApp/app/src/main/java/com/example/ra/oscarsapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.ra.oscarsapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -10,7 +9,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ListView mListview;
-    private ArrayList<ActorClass> mArrayList;
+    private ArrayList<ActorClass> mActorsArrayList;
 
 
     @Override
@@ -19,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mListview=(ListView)findViewById(R.id.lsitView);
-        mArrayList = new ArrayList<>();
+        mActorsArrayList = new ArrayList<>();
         ActorClass actorClass = ActorClass.getInstance();
-        mArrayList.add(new ActorClass("Leonardo DiCaprio", "11/11/74", "1"));
-        mArrayList.add(new ActorClass("Matt Damon", "10/8/70", "1"));
-        mArrayList.add(new ActorClass("Jennifer Lawrence", "08/15/90", "1"));
+        mActorsArrayList.add(new ActorClass("Denzel Washington", "11/11/74", "1"));
+        mActorsArrayList.add(new ActorClass("Jason Bourne", "10/8/70", "1"));
+        mActorsArrayList.add(new ActorClass("Jennifer Aniston", "08/15/90", "1"));
 
-        ActorArrayAdapter adapter = new ActorArrayAdapter(this, mArrayList);
+        ActorArrayAdapter adapter = new ActorArrayAdapter(this, mActorsArrayList);
         mListview.setAdapter(adapter);
     }
 }
